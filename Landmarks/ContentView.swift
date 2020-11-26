ced6a6d6e19a4a26f1f99121ab3dd4aea9d92f35
Naoyuki Furuta <nfrutta1208@gmail.com>
2020-11-27 08:11:00 +0900

    add sources

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by naoyuki on 2020/07/18.
+//  Copyright © 2020 naoyuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .font(.title)
+            .foregroundColor(Color.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
